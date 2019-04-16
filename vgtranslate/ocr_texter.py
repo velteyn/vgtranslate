383a1ab8c6135fbe96870298513590b843590964
Barry Rowe <barry@whoknows.com>
2019-04-16 13:57:20 -0700

    Added in missing tesseract files.

---
+++ vgtranslate/ocr_texter.py
@@ -0,0 +1,10 @@
+import pytesseract
+from imaging import segfil
+
+class OCRTexter:
+    @classmethod
+    def ocr_text(cls, image, colors, segcolor, lang=None):
+        #-outline:
+        # -reduce to multicolor
+        pass
+        
